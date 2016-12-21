c6e8a5e0883a3824b9a44106bf8516bf163143bb
Oluwasanmi Aderibigbe <AderibigbeOluwasanmi@gmail.com>
2016-12-21 15:44:07 +0100

    Started chapter four project

--- hello_world.rb
+++ hello_world.rb
@@ -1,9 +1,16 @@
 class HelloWorld
-  def Say
+  def say
     puts "Hello world"
   end
 end
 
 rubyHelloWorld = HelloWorld.new
 
+rubyHelloWorld.say
 
+
+rice_on_square = 1
+64.times do |square|
+  puts "On square #{square + 1} are #{rice_on_square} grain(s)"
+  rice_on_square *= 2
+end
