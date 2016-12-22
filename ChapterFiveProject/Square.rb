6dace1c3257df0dea50b28c6525f9a9a986395a9
Oluwasanmi Aderibigbe <AderibigbeOluwasanmi@gmail.com>
2016-12-22 12:45:13 +0100

    Learning oop

--- ChapterFiveProject/Square.rb
+++ ChapterFiveProject/Square.rb
@@ -1,17 +1,72 @@
 require_relative 'Shape'
 
-class Square < Shape
-  # @param [Fixnum] side_length
-  def initialize(side_length)
-    @side_length = side_length
+module Shapes
+  class Square < Shape
+
+    @@number_of_shapes = 0
+    Test = 2
+    # @param [Fixnum] side_length
+    def initialize(side_length)
+      @side_length = side_length
+      @@number_of_shapes += 1
+    end
+
+    # @return [Fixnum] area of square.
+    def calculate_area
+      @side_length * @side_length
+    end
+
+    def self.count
+      @@number_of_shapes
+    end
+
   end
 
-  # @return [Fixnum] area of square.
-  def calculate_area
-    @side_length * @side_length
+  class Rectangle < Shape
+    def initialize (length, breadth)
+      @length = length
+      @breadth = breadth
+
+    end
+
+    def calculate_area
+      @length * @breadth
+    end
   end
+
+  square1 = Square.new(5)
+  puts square1.calculate_area
+
+
+  square2 = Square.new(5)
+  puts square2.calculate_area
+
+
+  square4 = Square.new(5)
+  puts square4.calculate_area
+
+  square3 = Square.new(5)
+  puts square3.calculate_area
+
+  puts Square.count
+
+  rec1 = Rectangle.new(2, 3)
+  rec2 = Rectangle.new(4, 5)
+
+  animals = [square1, square2, square3, square4, rec1, rec2]
+
+  animals.each do |animal|
+    puts animal.calculate_area
+
+  end
+end
+
+class Testtheory
+  attr_accessor :name
 
 end
 
-square1 = Square.new(5)
-puts square1.calculate_area
+tt = Testtheory.new
+tt.name = 'sanmi'
+puts tt.name
+puts tt.instance_variables
