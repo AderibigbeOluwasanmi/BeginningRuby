455cde81444616e8f3b1b0e81e1f3df1d8c4802e
Oluwasanmi Aderibigbe <AderibigbeOluwasanmi@gmail.com>
2016-12-22 18:42:40 +0100

    Fix bugs  and implement a new feature that makes password generated contain two capital letters , thereby increasing the strength of the password.

--- PetProjects/PasswordGenerator.rb
+++ PetProjects/PasswordGenerator.rb
@@ -4,4 +4,4 @@ pass_gen = Sanmi_Utilities::PasswordGenerator.new
 
 pass_gen.collect_user_data
 p pass_gen.generate_password
-p pass_gen.time_stamp
+puts pass_gen
--- PetProjects/password_generator.rb
+++ PetProjects/password_generator.rb
@@ -1,36 +1,46 @@
 =begin
-  A password generator that generates a password six digit password with two uppercases and two digits
+  A password generator that generates a six digit password with two uppercase and two digits
 =end
 module Sanmi_Utilities
   class PasswordGenerator
+
     attr_accessor :first_name, :last_name
     attr_reader :password, :time_stamp
 
     public
     def collect_user_data
+      #TODO implement error handling.
       print 'Enter your first name: '
       @first_name = gets.downcase.chop.scan(/./)
 
       print 'Enter your second name: '
       @last_name = gets.downcase.chop.scan(/./)
-
     end
-
 
     def generate_password
       digits = '1234567890'.scan(/./)
       @password = ''
 
       2.times do
-        @password += @first_name[rand(@first_name.length - 1)]
-        @password += @last_name[rand(@last_name.length - 1)]
-        @password += digits[rand(9)]
+        @password += @first_name[rand(@first_name.length)]
+        @password += @last_name[rand(@last_name.length)]
+        @password += digits[rand(10)]
       end
 
       @time_stamp = Time.now.ctime
+
+      @password[0] = @password[0].upcase
+      @password[3] = @password[3].upcase
+
       @password
     end
 
+    def to_s
+      "Password generated: #{@password} Time stamp: #{@time_stamp}"
+    end
+
+    #TODO implement saving to file.
+    #TODO implement saving to database.
   end
 end
 
